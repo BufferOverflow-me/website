8f78822d74cd150846c96dfc1374daa557e7d7f3
YadavGulshan <yadavgulshan542@gmail.com>
2021-12-21 16:59:58 +0530

    added a navbar

--- components/landing_page/layout.jsx
+++ components/landing_page/layout.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Header from './widgets/header'
 import Footer from './widgets/footer'
 
 function LandingPageLayout(props) {
     return (
         <>
-            <Header />
             {props.children}
             <Footer />
         </>
--- components/landing_page/widgets/header.jsx
+++ components/landing_page/widgets/header.jsx
@@ -4,30 +4,32 @@ import styles from '../../../styles/Home.module.scss'
 function header() {
     return (
         <header className={styles.header}>
-                <Link href="/" passHref>
-                    <span className={styles.logo}>
-                        BufferOverflow
-                    </span>
+            <Link href="/" passHref>
+                <span className={styles.logo}>
+                    BufferOverflow
+                </span>
+            </Link>
+            <nav className={styles.mid}>
+                <Link href="/#Overview" passHref>
+                    Overview
                 </Link>
-                <nav className={styles.url}>
-                    <Link href="/#Overview" passHref>
-                        Overview
+                <Link href="/blog" passHref>
+                    Blog
+                </Link>
+                <Link href="/support" passHref>
+                    Support
+                </Link>
+            </nav>
+            <nav>
+                <Link href="/login" passHref>
+                    Login
+                </Link>
+                <button>
+                    <Link href="/signup" passHref>
+                            Create Account
                     </Link>
-                    <Link href="/blog" passHref>
-                        Blog
-                    </Link>
-                    <Link href="/support" passHref>
-                        Support
-                    </Link>
-                </nav>
-                <nav>
-                    <div className={styles.url}>
-                        Login in
-                    </div>
-                    <div className={styles.signup}>
-                        Create Account
-                    </div>
-                </nav>
+                </button>
+            </nav>
         </header>
     )
 }
