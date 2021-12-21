e428cc87150061f6838910ce4b558357af07e8c6
rahul007-bit <yadavrahul8141@yahoo.com>
2021-12-21 21:50:20 +0530

    Trying to add imagegit add -A

--- components/landing_page/screen/main.jsx
+++ components/landing_page/screen/main.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from '../../../styles/Home.module.scss'
+const myLoader = () => {
+    return `https://images.unsplash.com/photo-1503437313881-503a91226402?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODI5NXwwfDF8c2VhcmNofDF8fGRvJTIwd2hhdCUyMGlzJTIwZ3JlYXR8ZW58MHx8fHwxNjQwMDk1Mjk3&ixlib=rb-1.2.1&q=80&w=400`
+  }
 const main = () => {
     return (
         <div className={styles.mainSection}>
             <div className={styles.title}>
-
+                <blockquote>
                 A place to build great services.
+                </blockquote>
 
             </div>
             <div className={styles.desc}>
             Ullam asperiores necessitatibus nisi hic ullam ad sed voluptas. Porro ut accusamus earum soluta sint nemo.
             </div>
             <button href="#">Subscribe for updates</button>
+            <div className={styles.img}>
+                <Image
+                src='https://images.unsplash.com/photo-1503437313881-503a91226402?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODI5NXwwfDF8c2VhcmNofDF8fGRvJTIwd2hhdCUyMGlzJTIwZ3JlYXR8ZW58MHx8fHwxNjQwMDk1Mjk3&ixlib=rb-1.2.1&q=80&w=400'
+                alt='Home Image'
+                width={400}
+                height={80}
+                />
+            </div>
         </div>
     )
 }
