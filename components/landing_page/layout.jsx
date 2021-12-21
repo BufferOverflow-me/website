507849ed988038e05dd4352a05c36f64f41a2911
YadavGulshan <yadavgulshan542@gmail.com>
2021-12-21 14:47:08 +0530

    Setted up file str

---
+++ components/landing_page/layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function LandingPageLayout(props) {
+    return (
+        <>
+        </>
+    )
+}
+
+LandingPageLayout.propTypes = {
+
+}
+
+export default LandingPageLayout
+
---
+++ components/landing_page/widgets/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../../../styles/Home.module.scss'
+function header() {
+    return (
+        <header className={styles.header}>
+            <span className={styles.logo}>
+                BufferOverflow
+            </span>
+            <div>
+                
+            </div>
+        </header>
+    )
+}
+
+export default header
