ae129c76bed28c2913df585599e040aa3a1d1f69
rahul007-bit <yadavrahul8141@yahoo.com>
2021-12-22 15:06:34 +0530

    Added cards in features section

--- components/landing_page/screen/feature.jsx
+++ components/landing_page/screen/feature.jsx
@@ -4,7 +4,68 @@ import React from 'react'
 const feature = () => {
     return (
         <div className={styles.featureSection}>
-            feature Page
+            <h1>
+            All the feature you want
+            </h1>
+            <p className={styles.desc}>
+            Ullam asperiores necessitatibus nisi hic ullam ad sed voluptas. Porro ut accusamus earum soluta sint nemo. Animi blanditiis exercitationem quisquam. Laudantium est ea corrupti.
+            </p>
+            <div className={styles.grid}>
+                <div className={styles.card}>
+                    <h2>
+                    Feature
+                    </h2>
+                    <hr className={styles.cardHr}/>
+                    <p>
+                    Eum quisquam voluptate quia eveniet. Nam sit voluptatem nihil.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                    Feature
+                    </h2>
+                    <hr className={styles.cardHr}/>
+                    <p>
+                    Eum quisquam voluptate quia eveniet. Nam sit voluptatem nihil.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                    Feature
+                    </h2>
+                    <hr className={styles.cardHr}/>
+                    <p>
+                    Eum quisquam voluptate quia eveniet. Nam sit voluptatem nihil.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                    Feature
+                    </h2>
+                    <hr className={styles.cardHr}/>
+                    <p>
+                    Eum quisquam voluptate quia eveniet. Nam sit voluptatem nihil.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                    Feature
+                    </h2>
+                    <hr className={styles.cardHr}/>
+                    <p>
+                    Eum quisquam voluptate quia eveniet. Nam sit voluptatem nihil.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                    Feature
+                    </h2>
+                    <hr className={styles.cardHr}/>
+                    <p>
+                    Eum quisquam voluptate quia eveniet. Nam sit voluptatem nihil.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
