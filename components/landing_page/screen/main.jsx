2dc0b0d96079a5a7ff981a3b84ad6b0153a033f4
rahul007-bit <yadavrahul8141@yahoo.com>
2021-12-22 08:59:41 +0530

    more issues to homepage

--- components/landing_page/screen/main.jsx
+++ components/landing_page/screen/main.jsx
@@ -8,8 +8,7 @@ const main = () => {
     return (
         <div className={styles.mainSection}>
             <div className={styles.title}>
-                A place to build great services.
-
+                A place to build great services
             </div>
             <div className={styles.desc}>
             Ullam asperiores necessitatibus nisi hic ullam ad sed voluptas. Porro ut accusamus earum soluta sint nemo.
