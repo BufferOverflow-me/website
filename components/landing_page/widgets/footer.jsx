abe60ca89c64db295dab2dd8b058d3ea3d72f365
rahul007-bit <yadavrahul8141@yahoo.com>
2021-12-24 16:47:31 +0530

    Fixed Design issue at mobile size

--- components/landing_page/widgets/footer.jsx
+++ components/landing_page/widgets/footer.jsx
@@ -3,6 +3,26 @@ import styles from '../../../styles/Home.module.scss'
 function footer() {
     return (
         <footer className={styles.footer}>
+            <div className={styles.Container}>
+                <div className={styles.leftdiv}>
+
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.col}>
+
+                    </div>
+                    <div className={styles.col}>
+
+                    </div>
+                    <div className={styles.col}>
+
+                    </div>
+                    <div className={styles.col}>
+
+                    </div>
+                </div>
+            </div>
+
             <hr />
             <a
                 href="https://BufferOverflow.me"
