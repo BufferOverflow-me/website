ef410fef5358ed61f6a64b1e82493e842ff74198
rahul007-bit <yadavrahul8141@yahoo.com>
2021-12-26 18:16:01 +0530

    Working on Login Page!! which is all most done

--- pages/sign_in.jsx
+++ pages/sign_in.jsx
@@ -3,29 +3,40 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import { useAuth } from '../context/authUserContext';
-import { Container, Row, Col, Button, Form, FormGroup, Label, Input, Alert } from 'reactstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Alert
+} from 'reactstrap';
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const router = useRouter();
   const { signInWithEmailAndPassword, signInwithGoogle } = useAuth();
 
-  const onSubmit = event => {
-    setError(null)
-    signInWithEmailAndPassword(email, password)
+  const onSubmit = (event) => {
+    setError(null);
+    signInWithEmailAndPassword(email, password);
     event.preventDefault();
   };
 
-  const google = event => {
-    setError(null)
-    signInwithGoogle('/hello')
+  const google = (event) => {
+    setError(null);
+    signInwithGoogle('/hello');
     event.preventDefault();
-  }
+  };
 
   return (
-    <Container className="text-center" style={{ padding: '40px 0px' }}>
+    <>
+      {/* <Container className="text-center" style={{ padding: '40px 0px' }}>
       <Row>
         <Col>
           <h2>Login</h2>
@@ -66,13 +77,75 @@ export default function Home() {
             </FormGroup>
             <FormGroup row>
               <Col>
-                No account? <Link href="/sign_up">Create one</Link>
+                No account?<Link href="/sign_up">Create one</Link>
               </Col>
             </FormGroup>
           </Form>
           Sign In with google <Button onClick={google}>Google</Button>
         </Col>
       </Row>
-    </Container>
-  )
+    </Container> */}
+
+      <div className="h-screen flex flex-col items-center justify-center backdrop-blur-sm bg-white/30">
+        <h1 className="text-2xl font-semibold  leading-loose my-10">
+          Login to BufferOverflow
+        </h1>
+        <form className="p-3 text-center ">
+          <input
+            aria-label="Enter your email address"
+            type="text"
+            placeholder="Email address"
+            className="text-sm text-gray-base w-full
+                          py-5 px-4 h-2 border
+                        border-gray-200 rounded mb-2"
+          />
+
+          <input
+            aria-label="Enter your password"
+            type="password"
+            placeholder="Password"
+            className="text-sm text-gray-base w-full
+                        py-5 px-4 h-2 border border-gray-200
+                        rounded mb-2"
+          />
+
+          <button
+            type="submit"
+            className="bg-green-400 w-full mt-4 text-white p-2 rounded-md"
+          >
+            Login
+          </button>
+          <hr className="hr text-center mx-auto my-5 w-3/4 hr border-0 border-b-4 rounded-t-2 divider " />
+          <a  href="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              style={{fill:'#000000'}}
+            >
+              <path
+                fill="#FFC107"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+              <path
+                fill="#FF3D00"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              ></path>
+              <path
+                fill="#4CAF50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              ></path>
+              <path
+                fill="#1976D2"
+                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+            </svg>
+          </a>
+        </form>
+      </div>
+    </>
+  );
 }
