42ebc8e334cf16d02082d4696ab7fba6912cd529
rahul007-bit <yadavrahul8141@yahoo.com>
2021-12-26 22:38:02 +0530

    Add few more icon

--- pages/sign_in.jsx
+++ pages/sign_in.jsx
@@ -36,114 +36,101 @@ export default function Home() {
 
   return (
     <>
-      {/* <Container className="text-center" style={{ padding: '40px 0px' }}>
-      <Row>
-        <Col>
-          <h2>Login</h2>
-        </Col>
-      </Row>
-      <Row style={{ maxWidth: '400px', margin: 'auto' }}>
-        <Col>
-          <Form onSubmit={onSubmit}>
-            {error && <Alert color="danger">{error}</Alert>}
-            <FormGroup row>
-              <Label for="loginEmail" sm={4}>Email</Label>
-              <Col sm={8}>
-                <Input
-                  type="email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                  name="email"
-                  id="loginEmail"
-                  placeholder="Email" />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="loginPassword" sm={4}>Password</Label>
-              <Col sm={8}>
-                <Input
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                  id="loginPassword"
-                  placeholder="Password" />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Col>
-                <Button>Login</Button>
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Col>
-                No account?<Link href="/sign_up">Create one</Link>
-              </Col>
-            </FormGroup>
-          </Form>
-          Sign In with google <Button onClick={google}>Google</Button>
-        </Col>
-      </Row>
-    </Container> */}
-
-      <div className="h-screen flex flex-col items-center justify-center backdrop-blur-sm bg-white/30">
-        <h1 className="text-2xl font-semibold  leading-loose my-10">
-          Login to BufferOverflow
-        </h1>
-        <form className="p-3 text-center ">
+      <div className="h-screen flex flex-col items-center justify-center">
+        <form className="py-10 px-6 text-center bg-[#222222] rounded-md ">
+          <h1 className="text-2xl font-semibold  leading-loose my-10 text-cyan-50">
+            Login to BufferoverFlow
+          </h1>
           <input
             aria-label="Enter your email address"
             type="text"
             placeholder="Email address"
-            className="text-sm text-gray-base w-full
+            className="text-sm text-gray-base w-3/4 my-4
                           py-5 px-4 h-2 border
-                        border-gray-200 rounded mb-2"
+                        border-[#222] rounded mb-2"
           />
 
           <input
             aria-label="Enter your password"
             type="password"
             placeholder="Password"
-            className="text-sm text-gray-base w-full
-                        py-5 px-4 h-2 border border-gray-200
+            className="text-sm text-gray-base w-3/4 my-4
+                        py-5 px-4 h-2 border border-[#222]
                         rounded mb-2"
           />
 
           <button
             type="submit"
-            className="bg-green-400 w-full mt-4 text-white p-2 rounded-md"
+            className="bg-green-400 w-3/4 my-4 mt-4 text-green-900 p-2 rounded-md"
           >
             Login
           </button>
-          <hr className="hr text-center mx-auto my-5 w-3/4 hr border-0 border-b-4 rounded-t-2 divider " />
-          <a onClick={google}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="48"
-              height="48"
-              viewBox="0 0 48 48"
-              style={{fill:'#000000'}}
-            >
-              <path
-                fill="#FFC107"
-                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-              ></path>
-              <path
-                fill="#FF3D00"
-                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-              ></path>
-              <path
-                fill="#4CAF50"
-                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-              ></path>
-              <path
-                fill="#1976D2"
-                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-              ></path>
-            </svg>
-          </a>
+          <hr className="hr text-center mx-auto my-9 w-3/4 hr border-0 border-b-4 rounded-t-2 divider " />
+
+          <div className="flex flex-row justify-center">
+            <a onClick={google} className="mx-4 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="48"
+                height="48"
+                viewBox="0 0 48 48"
+                style={{ fill: '#000000' }}
+              >
+                <path
+                  fill="#FFC107"
+                  d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                ></path>
+                <path
+                  fill="#FF3D00"
+                  d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                ></path>
+                <path
+                  fill="#4CAF50"
+                  d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                ></path>
+                <path
+                  fill="#1976D2"
+                  d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                ></path>
+              </svg>
+            </a>
+            <a href="" className="mx-4 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="48"
+                height="48"
+                viewBox="0 0 30 30"
+                style={{ fill: '#fff' }}
+              >
+                {' '}
+                <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+              </svg>
+            </a>
+            <a href="" className="mx-4 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="48"
+                height="48"
+                viewBox="0 0 48 48"
+                style={{fill:'#000000'}}
+              >
+                <path
+                  fill="#039be5"
+                  d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"
+                ></path>
+              </svg>
+            </a>
+          </div>
         </form>
       </div>
     </>
